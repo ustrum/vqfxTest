cd5f211bc4301e4c424fcea830ca1d831d974695
Pablo Martin <ustrum@gmail.com>
2019-01-11 11:00:57 +0100

    Added git install for srv1

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@
 chmod 400 /home/vagrant/.ssh/id_rsa
 chmod 400 /home/vagrant/.ssh/id_rsa.pub
 chmod 400 /home/vagrant/.ssh/known_hosts
+sudo yum install git
 sudo yum install ansible -y
 sudo yum install epel-release -y
 sudo yum install python-pip -y
