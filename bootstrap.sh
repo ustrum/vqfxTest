307ad1f6d8866e2d388f39ad0bf94779e55df4e2
Pablo Salvador Martin <PabloSalvador.Martin@user.arinso>
2019-03-20 19:33:52 +0100

    Updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 chmod 400 /home/vagrant/.ssh/id_rsa
 chmod 400 /home/vagrant/.ssh/id_rsa.pub
 chmod 400 /home/vagrant/.ssh/known_hosts
-sudo yum install git
+sudo yum install git -y
 sudo yum install ansible -y
 sudo yum install epel-release -y
 sudo yum install python-pip -y
@@ -11,3 +11,4 @@ sudo pip install --upgrade pip
 sudo pip install jxmlease
 sudo pip install junos-eznc
 sudo ansible-galaxy install Juniper.junos
+sudo cp /home/vagrant/.ssh/id_rsa.pub /home/vagrant/.ssh/authorized_keys
